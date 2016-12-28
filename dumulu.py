5a34065b8a960bd1c84d7c0f5484850c8821fd24
zsy32 <291108451@qq.com>
2016-12-28 11:47:41 +0800

    Add files via upload

---
+++ dumulu.py
@@ -0,0 +1,49 @@
+import os
+
+
+def ListFilesToTxt(dir, file, wildcard, recursion):
+    exts = wildcard.split(" ")
+    files = os.listdir(dir)
+    for name in files:
+        fullname = os.path.join(dir, name)
+        if (os.path.isdir(fullname) & recursion):
+            ListFilesToTxt(fullname, file, wildcard, recursion)
+        else:
+            for ext in exts:
+                if (name.endswith(ext)):
+                    file.write(name + "\n")
+                    break
+
+
+def Test():
+    dir = "/home/zhenshiziben/pycharm_installation/pdf_to_txt"
+    outfile = "hello.txt"
+    wildcard = ".txt .exe .dll .lib"
+
+    file = open("/home/zhenshiziben/pycharm_installation/pdf_to_txt/hello1.txt", "r")
+    # if not file:
+        # print("cannot open the file %s for writing" % outfile)
+
+    # ListFilesToTxt(dir, file, wildcard, 1)
+    temp_list = []
+    content_list = file.readlines()
+    j = 0
+    temp = 0
+    for c in content_list:
+        j += 1
+        if c.strip() == "内容目录":
+            temp = j
+            continue
+        elif c.strip() == "图表目录":
+            break
+    print(j)
+    print(temp)
+    for i in range(temp,j):
+        print(content_list[i].strip())
+    # str_temp = file.readline()
+    # print(temp_list)
+
+    file.close()
+
+
+Test()
